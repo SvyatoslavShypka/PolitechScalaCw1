1424284b1a6a2eb3c87f530701fb0c1186c1fd82
Svyatoslav Shypka <svyat200909@gmail.com>
2023-10-30 07:53:48 +0100

    All 7

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,7 @@
 object Main {
 
-
   //Zad.1
-  def flatten1[A](list1: List[List[A]]): List[A] = {
+  private def flatten1[A](list1: List[List[A]]): List[A] = {
 
     if (list1 != null && list1.nonEmpty) {
       list1.head ++ flatten1(list1.tail)
@@ -10,7 +9,7 @@ object Main {
   }
 
   //Zad.2
-  def count[A](x: A, list: List[A]): Int = {
+  private def count[A](x: A, list: List[A]): Int = {
     if (list != null && list.nonEmpty) {
       val helpcount = if (x == list.head) 1 else 0
       helpcount + count(x, list.tail)
@@ -18,38 +17,34 @@ object Main {
   }
 
   //Zad.3
-
-  def replicate[A](x: A, num: Int): List[A] = {
+  private def replicate[A](x: A, num: Int): List[A] = {
     if (num > 0) {
       List(x) ++ replicate(x, num - 1)
     } else Nil
   }
 
   //Zad.4
-
-  def sqrList(list: List[Int]): List[Int] = {
+  private def sqrList(list: List[Int]): List[Int] = {
     if (list != null && list.nonEmpty) {
       List(list.head * list.head) ++ sqrList(list.tail)
     } else Nil
   }
 
   //Zad.5
-
-  def palindrome[A](list: List[A]): Boolean = {
+  private def palindrome[A](list: List[A]): Boolean = {
     if (list == list.reverse) {true}
     else false
   }
 
   //Zad. 6
-
-  def listLength[A](list: List[A]): Int = {
+  private def listLength[A](list: List[A]): Int = {
     if (list != null && list.nonEmpty) {
       1 + listLength(list.tail)
     } else 0
   }
 
   //Zad. 7
-  def Equation(N: Double): Double = {
+  private def Equation(N: Double): Double = {
     val c = 1
     if (N == 1) 1
 //    else (c * math.log(N)) / math.log(2) + Equation(N / 2)
@@ -58,7 +53,6 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     println(flatten1(List(List(5, 6), List(1, 2, 3))))
-
     println(count("a", List("a", "l", "a")))
     println(replicate("la", 5))
     println(sqrList(List(1, 2, 3, 4, 5, 6)))
@@ -72,5 +66,4 @@ object Main {
     println(Equation(4))
     println(Equation(8))
   }
-
 }
